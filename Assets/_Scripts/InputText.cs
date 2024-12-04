366cc0f179e4cbc8afd046941bdf2a1cee748c67
KingKrio <jaycool2@student.ubc.ca>
2024-12-04 02:57:11 -0800

    fixed feature 3 bugs

---
+++ Assets/_Scripts/InputText.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputText : MonoBehaviour
+{
+    public TextMeshProUGUI code;
+    public GameObject screen;
+    public GameObject email;
+    //public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        code = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(code.text.Length == 3)
+        {
+            if(code.text.Equals("476"))
+            {
+                email.SetActive(true);
+                screen.SetActive(false);
+            }
+            else
+            {
+                code.text = "";
+            }
+        }
+    }
+
+    public void ButtonOnPressed(string text)
+    {
+        code.text = code.text + text;
+    }
+}
